3de91e239f63ede7cc793d962444cf4c526f08de
梅林的法杖 <530264127@qq.com>
2021-09-09 15:31:26 +0800

    feat: vite demo

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HelloWorld msg="Vue3 + Vite" />
+  <div class="avatar"></div>
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+fetch("/api/todos/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+</script>
+
+<style scoped>
+.avatar {
+  width: 200px;
+  height: 200px;
+  background-image: url(@/assets/avatar.png);
+  background-size: contain;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <div class="postcss"></div>
+  <button @click="handleClick">{{state.count}}</button>
+</template>
+
+<script setup lang="ts">
+import { reactive, defineProps } from 'vue'
+defineProps({
+  msg: String
+})
+interface State {
+  count: number;
+}
+let state = reactive<State>({count: 0});
+const handleClick = () => {
+  console.log(state.count);
+  state.count++
+}
+</script>
+
+<style scoped>
+.postcss {
+  height: 50px;
+  width: 200px;
+  background-color: orange;
+  transform: rotate(90deg);
+}
+</style>
